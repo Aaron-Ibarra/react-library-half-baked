b9e99785af5471f33ae37e4444f21dd43f451c60
Aaron Ibarra <aaronibarra132@gmail.com>
2023-01-24 17:56:11 -0800

    added a redirect from the home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import BookList from './components/book/BookList';
 import BookDetail from './components/book/BookDetail.js';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <main className="container">
       <h1>Library Catalog</h1>
       <Switch>
+        <Route exact path="/">
+          <Redirect to="/books" />
+        </Route>
         <Route exact path="/books" component={BookList} />
         <Route exact path="/books/:id" component={BookDetail} />
       </Switch>
